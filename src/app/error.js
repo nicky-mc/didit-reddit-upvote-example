3e03e2e95c253d18c5b2ff6497575e5f60198d5b
nicky-mc <cinimod@gmail.com>
2024-11-06 14:57:45 +0000

    error.js

--- src/app/error.js
+++ src/app/error.js
@@ -2,14 +2,14 @@
 
 import Link from "next/link";
 
-export default function GlobalError() {
+export default function GlobalError ( { reset } ) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-error mb-4">Naughty Naughty you probably should log in</h1>
-        <Link href="/">
-          <a className="btn btn-primary">Return Home</a>
-        </Link>
+        <button onClick={() => reset()} className="btn btn-primary">
+          Try again
+        </button>}
       </div>
     </div>
   );
