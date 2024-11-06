a67a1a06c5aea1b5a3a3a66112d71a7de22a739a
nicky-mc <cinimod@gmail.com>
2024-11-06 14:29:16 +0000

    error.js

---
+++ src/app/error.js
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+
+export default function GlobalError() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-error mb-4">Global Error</h1>
+        <Link href="/" className="btn btn-primary">
+          Return Home
+        </Link>
+      </div>
+    </div>
+  );
+}
