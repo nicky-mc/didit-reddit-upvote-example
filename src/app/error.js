2e1f87aeeda50256e0ab22727851a564a7f45e0f
nicky-mc <cinimod@gmail.com>
2024-11-06 15:08:11 +0000

    error.js

--- src/app/error.js
+++ src/app/error.js
@@ -1,14 +1,27 @@
 "use client";
+import Image from "next/image";
 
-import Link from "next/link";
-
-export default function GlobalError ( { reset } ) {
+export default function GlobalError({ error, reset }) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-error mb-4">Naughty Naughty you probably should log in</h1>
+        <Image
+          src="/naughty.jpg"
+          alt="Naughty Warning"
+          width={300}
+          height={300}
+          className="rounded-full mb-6 shadow-lg"
+        />
+        <h1 className="text-4xl font-bold text-error mb-4">
+          Naughty Naughty! You probably should log in.
+        </h1>
+        {error && (
+          <p className="text-lg text-gray-500 mb-4">
+            {error.message || "this was not the error you were looking for."}
+          </p>
+        )}
         <button onClick={() => reset()} className="btn btn-primary">
-          Try again
+          Try Again
         </button>
       </div>
     </div>
