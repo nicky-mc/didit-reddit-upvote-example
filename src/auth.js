17dadc64681d723795122d98c5808f5a8d786cbb
nicky-mc <cinimod@gmail.com>
2024-11-06 15:29:25 +0000

    error.js

--- src/auth.js
+++ src/auth.js
@@ -8,20 +8,9 @@ export const { auth, handlers, signOut, signIn } = NextAuth({
   providers: [GitHub],
   trustHost: true,
   callbacks: {
-  session: async ({ session, token }) => {
-    session.user.id = token.id;
-    return session;
-  },
-},
-cookies: {
-  sessionToken: {
-    name: `__Secure-next-auth.session-token`,
-    options: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      path: "/",
-      sameSite: "strict",
+    session: async ({ session, user }) => {
+      session.user.id = user.id;
+      return session;
     },
   },
-},
 });
