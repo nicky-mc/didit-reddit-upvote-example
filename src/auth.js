7e652eaa234303ec445378b5c184fd39f7e794b7
nicky-mc <cinimod@gmail.com>
2024-11-06 12:43:52 +0000

    checking title work around

--- src/auth.js
+++ src/auth.js
@@ -5,10 +5,17 @@ import { db } from "./db";
 
 export const { auth, handlers, signOut, signIn } = NextAuth({
   adapter: PostgresAdapter(db),
-  providers: [GitHub({
-    clientId: process.env.AUTH_GITHUB_ID,
-    clientSecret: process.env.AUTH_GITHUB_SECRET,
-  })],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user user:email",
+        },
+      },
+    }),
+  ],
   trustHost: true,
   callbacks: {
     session: async ({ session, user }) => {
