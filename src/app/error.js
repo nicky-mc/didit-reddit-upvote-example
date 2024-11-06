dc4c3ac7abb2b9ae5144fb18b2af88d8c206c63b
nicky-mc <cinimod@gmail.com>
2024-11-06 14:40:05 +0000

    error.js

--- src/app/error.js
+++ src/app/error.js
@@ -7,10 +7,8 @@ export default function GlobalError() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-error mb-4">Naughty Naughty you probably should log in</h1>
-        <Link href="/">
-          <button className="btn btn-primary">
-            Return Home
-          </button>
+        <Link href="/" className="btn btn-primary">
+          Return Home
         </Link>
       </div>
     </div>
